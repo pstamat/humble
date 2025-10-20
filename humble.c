a0d0189148566674b577d582ffda2b3daba8d180
Panagiotis Stamatopoulos <takis@di.uoa.gr>
2025-10-20 10:18:58 +0300

    Working version

--- humble.c
+++ humble.c
@@ -1,8 +1,35 @@
-/* File: humble.c *.
+/* File: humble.c */
+
+/* Finds all humble numbers in a given range */
+
+/* A number is humble if it doesn't have any prime
+   factors different from 2, 3, 5, 7  */
 
 #include <stdio.h>
+#define MINNUM 10
+#define MAXNUM 1000
 
 int main()
 {
-  return 0;
+	int num, temp;
+	for (num = MINNUM ; num <= MAXNUM ; num++) {
+		temp = num;
+		while (temp % 2 == 0) {
+			temp /= 2;
+		}
+		while (temp % 3 == 0) {
+                        temp /= 3;
+                }
+		while (temp % 5 == 0) {
+                        temp /= 5;
+                }
+		while (temp % 7 == 0) {
+                        temp /= 7;
+                }
+		if (temp == 1) {
+			printf("%d is a humble number\n", num);
+		}
+	}
+
+	return 0;
 }
